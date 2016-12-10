e15f458746c619ef7398ad7358b8982762939415
egarciavaz <egarcia@boundify.io>
2016-12-10 01:44:02 -0500

    JavaScript

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+// alert("Hello World!");
+$(document).ready(function() {
+  var $menu = $("#sidebar-wrapper");
+
+  $(document).on("click", ".js-menu-open", function() {
+    $menu.addClass("open");
+    return false;
+  });
+
+  $(document).on("click", ".js-menu-close", function() {
+    $menu.removeClass("open");
+    return false;
+  });
+});
